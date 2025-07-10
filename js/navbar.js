5fb6db9f4fef932f34a040b0c6581ce8322dee7f
Vannos-Slid <ivan.kush05@gmail.com>
2025-07-10 20:07:38 +0300

    navbar disapear

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+function showSidebar() {
+    var element = document.getElementById("myDIV")
+    element.classList.toggle("show")
+
+}
+
+function hideSidebar() {
+    var element = document.getElementById("myDIV")
+    element.classList.toggle("show")
+}
