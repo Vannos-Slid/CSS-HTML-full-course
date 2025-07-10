92238fdc3c9a9fd0707a10b7249c8cfe0605bdec
Vannos-Slid <ivan.kush05@gmail.com>
2025-07-10 20:18:45 +0300

    navbar inert attribute

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,12 @@
 function showSidebar() {
     var element = document.getElementById("myDIV")
     element.classList.toggle("show")
+    element.removeAttribute('inert')
 
 }
 
 function hideSidebar() {
     var element = document.getElementById("myDIV")
     element.classList.toggle("show")
+    element.setAttribute('inert', '')
 }
