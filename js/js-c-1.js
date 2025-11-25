ffff1b26bb912b5ffcd81ac3f5140e41b1e0b4b6
Vannos-Slid <ivan.kush05@gmail.com>
2025-11-25 21:47:01 +0200

    variables

--- js/js-c-1.js
+++ js/js-c-1.js
@@ -1,17 +1,41 @@
-for (let i = 1; i <= 5; ++i) {
-    console.log("Count", i);
-}
+// #1
+// for (let i = 1; i <= 5; ++i) {
+//     console.log("Count", i);
+// }
 
-let score = 85;
+// let score = 85;
 
-const conLog = (text) => {
-    console.log(text);
-}
+// const conLog = (text) => {
+//     console.log(text);
+// }
 
-if (score >= 90) {
-    conLog("Grade: A");
-} else if (score >= 80) {
-    conLog("Grade: B");
-} else {
-    conLog("Grade: C or lower");
-}
+// if (score >= 90) {
+//     conLog("Grade: A");
+// } else if (score >= 80) {
+//     conLog("Grade: B");
+// } else {
+//     conLog("Grade: C or lower");
+// }
+
+// #2
+// console.log(username); // error
+
+// let username = "Tom";
+// console.log(username);
+// username = "Harry";
+// console.log(username);
+// username = "Ron";
+// console.log(username);
+
+// #4
+const username = "Tom";
+// username = "Harry"; // error
+
+// #5
+let a;
+let color;
+let _private;
+let $button;
+let getTop10;
+let a_large_name;
+let camelCase;
